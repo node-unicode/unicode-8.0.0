783e79b6b91ae50837fbb750ef884ff265c05af3
Mathias Bynens <mathias@qiwi.be>
2024-11-08 15:15:18 +0100

    Release v1.6.4

--- Names/Figment/index.js
+++ Names/Figment/index.js
@@ -1,1 +1,1 @@
-module.exports=JSON.parse(require('zlib').gunzipSync(Buffer.from('H4sIAAAAAAAAE6tWMjSyULKKVgpwdHHx9HNXcPZwDHJ0DnENUorVAcpZguQ8PN09FPyBgiEKAUGuwa4hYDlTY5BcMFCTj6uCe5BjgIenM0K7gqdfSJC/S6gzyKRaAEnBiKVnAAAA','base64')))
+module.exports=JSON.parse(require('zlib').gunzipSync(Buffer.from('H4sIAAAAAAAAE0XMMQqAMAxA0auEzC6tCOpW0tAGpC0xTuJJxLtLXZwf/9/o/IzriS3EKCUB5aCBjBWvAZ1fumVJGSoZGzTlne2zaey2S0kbQ9LQstCfgxTTGg/qp+cFScGIpWcAAAA=','base64')))
